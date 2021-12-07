7c77b1cc101840e3492eae4666f094b9fc35631b
William Du <70287641+W1ll-Du@users.noreply.github.com>
2021-12-07 10:16:35 -0500

    Create StatsCommand.java

---
+++ app/src/main/java/io/github/w1ll_du/whitelister/command/commands/StatsCommand.java
@@ -0,0 +1,25 @@
+package io.github.w1ll_du.whitelister.command.commands;
+
+import io.github.w1ll_du.whitelister.command.CommandContext;
+import io.github.w1ll_du.whitelister.command.ICommand;
+import io.github.w1ll_du.whitelister.Utils;
+import java.util.List;
+
+public class StatsCommand implements ICommand {
+    @Override
+    public void handle(CommandContext ctx) {
+        // [username] [statistic name]
+        String cmd;
+        ctx.getChannel().sendMessage(Utils.rconCommand(cmd)).queue();
+    }
+
+    @Override
+    public String getName() {
+        return "stats";
+    }
+    
+    @Override
+    public List<String> getAliases() {
+        return List.of("statistics");
+    }
+}
