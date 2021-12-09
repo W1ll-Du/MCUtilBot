5ac4106588fae6a833a9bb173315d1cc386375de
William Du <duwill1412@gmail.com>
2021-12-09 01:02:46 -0500

    int to float

--- app/src/main/java/io/github/w1ll_du/MCUtilsBot/command/commands/cmToMCommand.java
+++ app/src/main/java/io/github/w1ll_du/MCUtilsBot/command/commands/cmToMCommand.java
@@ -2,15 +2,12 @@ package io.github.w1ll_du.MCUtilsBot.command.commands;
 
 import io.github.w1ll_du.MCUtilsBot.command.CommandContext;
 import io.github.w1ll_du.MCUtilsBot.command.ICommand;
-import net.dv8tion.jda.api.JDA;
-
-import java.util.List;
 
 public class cmToMCommand implements ICommand {
 
     @Override
     public void handle(CommandContext ctx) {
-        ctx.getChannel().sendMessage(String.valueOf(Integer.valueOf(ctx.getArgs().get(0)) / 100)).queue();
+        ctx.getChannel().sendMessage(String.valueOf(Integer.parseInt(ctx.getArgs().get(0)) / 100.0)).queue();
     }
 
     @Override
--- app/src/main/java/io/github/w1ll_du/MCUtilsBot/command/commands/ticksToMinutes.java
+++ app/src/main/java/io/github/w1ll_du/MCUtilsBot/command/commands/ticksToMinutes.java
@@ -2,13 +2,12 @@ package io.github.w1ll_du.MCUtilsBot.command.commands;
 
 import io.github.w1ll_du.MCUtilsBot.command.CommandContext;
 import io.github.w1ll_du.MCUtilsBot.command.ICommand;
-import net.dv8tion.jda.api.JDA;
 
 public class ticksToMinutes implements ICommand {
 
     @Override
     public void handle(CommandContext ctx) {
-        ctx.getChannel().sendMessage(String.valueOf(Integer.valueOf(ctx.getArgs().get(0)) / 1200)).queue();
+        ctx.getChannel().sendMessage(String.valueOf(Integer.parseInt(ctx.getArgs().get(0)) / 1200.0)).queue();
     }
 
     @Override
