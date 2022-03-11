be621e126e72807c4c8b800859625ffd0cad70e1
William Du <duwill1412@gmail.com>
2022-03-11 15:50:29 -0500

    add ARconCommand

--- app/src/main/java/io/github/w1ll_du/MCUtilsBot/command/commands/checkAFKCommand.java
+++ app/src/main/java/io/github/w1ll_du/MCUtilsBot/command/commands/checkAFKCommand.java
@@ -1,12 +1,12 @@
 package io.github.w1ll_du.MCUtilsBot.command.commands;
 
 import io.github.w1ll_du.MCUtilsBot.Utils;
+import io.github.w1ll_du.MCUtilsBot.command.ARconCommand;
 import io.github.w1ll_du.MCUtilsBot.command.CommandContext;
-import io.github.w1ll_du.MCUtilsBot.command.ICommand;
 
-public class checkAFKCommand implements ICommand {
+public class checkAFKCommand extends ARconCommand {
     @Override
-    public void handle(CommandContext ctx) {
+    protected void handle2(CommandContext ctx) {
         ctx.getChannel().sendMessage(Utils.rconCommand("team list afkDis.afk")).queue();
     }
 
--- app/src/main/java/io/github/w1ll_du/MCUtilsBot/command/commands/rconCommand.java
+++ app/src/main/java/io/github/w1ll_du/MCUtilsBot/command/commands/rconCommand.java
@@ -3,9 +3,16 @@ package io.github.w1ll_du.MCUtilsBot.command.commands;
 import io.github.w1ll_du.MCUtilsBot.Utils;
 import io.github.w1ll_du.MCUtilsBot.command.AAdminCommand;
 import io.github.w1ll_du.MCUtilsBot.command.CommandContext;
+import me.dilley.MineStat;
 
 public class rconCommand extends AAdminCommand  {
     protected void handle2(CommandContext ctx) {
+        // simulate ARconCommand
+        if (!new MineStat(ctx.getConf().get("mc_server_ip"), 25565).isServerUp()) {
+            ctx.getChannel().sendMessage("Could not execute command at the time.").queue();
+            return;
+        }
+
         ctx.getChannel().sendMessage(Utils.rconCommand(Utils.compress(ctx.getArgs()))).queue();
     }
 
