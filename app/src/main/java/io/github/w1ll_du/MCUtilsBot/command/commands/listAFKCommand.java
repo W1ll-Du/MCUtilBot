308a8e7c42a1b5fd5d4deeadf224706b4a96daab
William Du <duwill1412@gmail.com>
2022-03-11 18:08:04 -0500

    rename command

--- app/src/main/java/io/github/w1ll_du/MCUtilsBot/command/commands/listAFKCommand.java
+++ app/src/main/java/io/github/w1ll_du/MCUtilsBot/command/commands/listAFKCommand.java
@@ -4,7 +4,7 @@ import io.github.w1ll_du.MCUtilsBot.Utils;
 import io.github.w1ll_du.MCUtilsBot.command.ARconCommand;
 import io.github.w1ll_du.MCUtilsBot.command.CommandContext;
 
-public class checkAFKCommand extends ARconCommand {
+public class listAFKCommand extends ARconCommand {
     @Override
     protected void handle2(CommandContext ctx) {
         ctx.getChannel().sendMessage(Utils.rconCommand("team list afkDis.afk")).queue();
